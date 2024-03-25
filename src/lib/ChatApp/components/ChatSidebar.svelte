f794a086ca58ffa7d19c27779aacbd88b20cdde2
Quangdao Nguyen <me@quangdao.com>
2024-03-25 00:12:54 -0500

    Clip display names

--- src/lib/ChatApp/components/ChatSidebar.svelte
+++ src/lib/ChatApp/components/ChatSidebar.svelte
@@ -2,7 +2,7 @@
 	import type { Bot } from '$lib/models/Bot';
 	import { createEventDispatcher } from 'svelte';
 
-  const dispatch = createEventDispatcher();
+	const dispatch = createEventDispatcher();
 
 	export let bots: Bot[];
 </script>
@@ -18,7 +18,9 @@
 							<img src={bot.profilePicture} alt={bot.name} />
 						</div>
 						<div class="bot-name">
-							{bot.name}
+							<span>
+								{bot.name}
+							</span>
 						</div>
 					</button>
 				</li>
@@ -28,14 +30,14 @@
 </div>
 
 <style lang="scss">
-  @use '~/theme';
+	@use '~/theme';
 	.chat-sidebar {
 		height: 100%;
 		background-color: var(--color-mid);
-    @include theme.light {
-      background-color: var(--color-white);
-      border-right: 0.25em solid var(--color-mid);
-    }
+		@include theme.light {
+			background-color: var(--color-white);
+			border-right: 0.25em solid var(--color-mid);
+		}
 	}
 
 	details {
@@ -45,13 +47,13 @@
 	summary {
 		padding: 0.5em;
 		cursor: pointer;
-    font-size: 1.2em;
+		font-size: 1.2em;
 	}
 
 	.bots {
 		list-style-type: none;
 		padding: 0;
-    margin: 0;
+		margin: 0;
 	}
 
 	.bot {
@@ -59,24 +61,29 @@
 			padding: 1em;
 			background: none;
 			border: none;
-			display: block;
+			display: flex;
 			width: 100%;
+			max-width: 20vw;
 			text-align: left;
+			gap: 0.5em;
+			align-items: center;
 			&:hover {
 				background: rgba(#000, 0.2);
 			}
 		}
-    &-avatar {
-      margin-right: 0.5em;
-      img {
-        width: 2em;
-        display: block;
-        border-radius: 50%;
-      }
-    }
-    &-avatar, &-name {
-      display: inline-block;
-      vertical-align: middle;
-    }
+		&-avatar {
+			flex: 0 0 auto;
+			img {
+				width: 2em;
+				display: block;
+				border-radius: 50%;
+			}
+		}
+		&-name {
+			flex: 1 1 0;
+			overflow: hidden;
+			white-space: nowrap;
+			text-overflow: ellipsis;
+		}
 	}
 </style>
