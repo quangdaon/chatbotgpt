8ca30c5600555ceae2fa9ffd553a90fec4df9abf
Quangdao Nguyen <qnguyen@nvisia.com>
2024-06-05 09:45:01 -0500

    Image upload

--- src/lib/ChatApp/components/Custom/CustomBotForm.svelte
+++ src/lib/ChatApp/components/Custom/CustomBotForm.svelte
@@ -5,6 +5,12 @@
 	const dispatch = createEventDispatcher();
 
 	let imgPreview: HTMLImageElement;
+	let upload: HTMLInputElement;
+
+	let botName: string;
+	let botAvatarUrl: string;
+	let botPrompt: string;
+	let useUpload = true;
 
 	const bot: BotCustom = {
 		type: 'custom',
@@ -15,7 +21,14 @@
 	};
 
 	const submit = () => {
-		bot.id = crypto.randomUUID();
+		const bot: BotCustom = {
+			type: 'custom',
+			id: crypto.randomUUID(),
+			name: botName,
+			profilePicture: getBase64Image(),
+			prompt: botPrompt
+		};
+
 		if (!bot.prompt) bot.prompt = tempPrompt;
 		bot.profilePicture = getBase64Image();
 		dispatch('submitted', bot);
@@ -23,18 +36,35 @@
 
 	function getBase64Image() {
 		var canvas = document.createElement('canvas');
+		const ratio = imgPreview.width / imgPreview.height;
 		canvas.width = 128;
-		canvas.height = imgPreview.height * (128 / imgPreview.width);
+		canvas.height = 128;
+		const imageWidth = ratio < 1 ? 128 : imgPreview.width * (128 / imgPreview.height);
+		const imageHeight = ratio > 1 ? 128 : imgPreview.height * (128 / imgPreview.width);
 
 		var ctx = canvas.getContext('2d');
-		ctx?.drawImage(imgPreview, 0, 0, canvas.width, canvas.height);
+		ctx?.drawImage(imgPreview, 0, 0, imageWidth, imageHeight);
 
 		var dataURL = canvas.toDataURL('image/png');
 
 		return dataURL;
 	}
 
-	$: tempPrompt = bot.name ? `You are ${bot.name}.` : '';
+	function onFileChange() {
+		const file = upload.files ? upload.files[0] : null;
+
+		if (file) {
+			const reader = new FileReader();
+			reader.addEventListener('load', function () {
+				botAvatarUrl = reader.result?.toString() ?? '';
+			});
+			reader.readAsDataURL(file);
+
+			return;
+		}
+	}
+
+	$: tempPrompt = botName ? `You are ${botName}.` : '';
 </script>
 
 <ChatHeader>
@@ -44,21 +74,40 @@
 <form on:submit|preventDefault={submit} class="form">
 	<div class="field">
 		<label for="bot-name">Name</label>
-		<input required id="bot-name" type="text" bind:value={bot.name} />
+		<input required id="bot-name" type="text" bind:value={botName} />
 	</div>
 
-	<div class="field">
-		<label for="bot-profile">Profile Picture URL</label>
-		<input required id="bot-profile" type="url" bind:value={bot.profilePicture} />
-	</div>
+	{#if useUpload}
+		<div class="field">
+			<label for="bot-upload">Upload</label>
+			<input
+				id="bot-upload"
+				type="file"
+				bind:this={upload}
+				on:change={onFileChange}
+				accept="image/*"
+			/>
+			<div class="avatar-toggle">
+				<button on:click={() => useUpload = false}>Enter URL Instead</button>
+			</div>
+		</div>
+	{:else}
+		<div class="field">
+			<label for="bot-profile">Profile Picture URL</label>
+			<input required id="bot-profile" type="url" bind:value={botAvatarUrl} />
+			<div class="avatar-toggle">
+				<button on:click={() => useUpload = true}>Upload Image Instead</button>
+			</div>
+		</div>
+	{/if}
 
-	{#if bot.profilePicture}
+	{#if botAvatarUrl}
 		<div>
 			<p>Image Preview:</p>
 			<div class="avatar-preview">
 				<img
 					bind:this={imgPreview}
-					src={bot.profilePicture}
+					src={botAvatarUrl}
 					alt="Preview of avatar"
 					crossOrigin="anonymous"
 				/>
@@ -68,7 +117,7 @@
 
 	<div class="field">
 		<label for="bot-prompt">Prompt</label>
-		<textarea name="bot-prompt" id="bot-prompt" placeholder={tempPrompt} bind:value={bot.prompt}
+		<textarea name="bot-prompt" id="bot-prompt" placeholder={tempPrompt} bind:value={botPrompt}
 		></textarea>
 	</div>
 
@@ -97,6 +146,9 @@
 				max-width: min(800px, 90%);
 			}
 		}
+		.avatar-toggle {
+			margin-top: 1em;
+		}
 		.avatar-preview {
 			width: 5em;
 			aspect-ratio: 1;
@@ -105,6 +157,8 @@
 			margin: 1em 0;
 			img {
 				width: 100%;
+				aspect-ratio: 1;
+				// object-fit: cover;
 				display: block;
 			}
 		}
